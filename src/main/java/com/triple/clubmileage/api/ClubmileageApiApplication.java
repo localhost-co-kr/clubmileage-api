645103ad95368214b75ce5476621621cc256229e
localhost-co-kr <127.0.0.1@localhost.co.kr>
2022-06-15 22:28:51 +0900

    Spring boot dependency 추가 (webflux, tester)

--- src/main/java/com/triple/clubmileage/api/ClubmileageApiApplication.java
+++ src/main/java/com/triple/clubmileage/api/ClubmileageApiApplication.java
@@ -1,2 +1,12 @@
-package com.triple.clubmileage.api;public class ClubmileageApiApplication {
+package com.triple.clubmileage.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubmileageApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClubmileageApiApplication.class, args);
+    }
 }
